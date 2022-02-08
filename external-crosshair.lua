33cf8d20dbe6925df3295160b145df6ba10bc97a
illegal-instruction <79029454+illegal-instruction-co@users.noreply.github.com>
2022-02-08 04:44:56 +0300

    Update external-crosshair.lua

--- external-crosshair.lua
+++ external-crosshair.lua
@@ -1,1 +1,17 @@
-FiveX.CreateXui("https://bit.ly/31zeq3Q", 1920, 1080)
+local xuiWidth, xuiHeight = GetActiveScreenResolution()
+local xuiHandle = FiveX.CreateXui("https://raw.githubusercontent.com/illegal-instruction-co/Xternal-Crosshair/main/index.html", xuiWidth, xuiHeight)
+
+--[[
+  Destroy XESP Thread
+--]]
+Citizen.CreateThread(function()
+  while true do
+    if activeXui then
+      if FiveX.IsKeyPressed(0x73) then -- F4
+          FiveX.DestroyXui(xuiHandle)
+          activeXui = false
+      end
+    end
+    Citizen.Wait(0)
+  end
+end)
